391cd8abb341f73dc6edce892e51be6f196e2fc5
sickwrtn <sillo154265@gmail.com>
2025-03-19 20:22:18 +0900

    db

--- src/error.tsx
+++ src/error.tsx
@@ -1,9 +1,14 @@
-
+import "./main.css"
 
 function Error(){
     return (
-    <>
-        <h1>ERROR PAGE</h1>
+    <> 
+        <div className="wrapper" >
+         <h1 id="db">점검 진행중</h1>
+        </div>
+        <div className="wrapper-in" >
+         <h1 id="db">예상 완료 시간: 9:00 PM</h1>
+        </div>
     </>
     )
 }
